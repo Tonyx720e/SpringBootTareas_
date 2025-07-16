4ed98e02cf554e2167266a8ed71d0de977b8bd55
Tony Medina <cayapadevelop@gmail.com>
2025-07-16 22:44:29 +0200

    primer comit para Spring Boot

---
+++ src/main/java/com/SpringBooteTarea/Tarea/DemoRestController.java
@@ -0,0 +1,17 @@
+package com.SpringBooteTarea.Tarea;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class DemoRestController {
+
+    @GetMapping("/")
+    public String getTarea(){
+        return "Esta es la terea de Spring Boot";
+
+    }
+@Value("${nombre}")
+    private String nombre;
+}
